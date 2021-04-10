1833d6bdb71a61fa8093cb0d4cd076d6af1d1351
Claire Lemonnier <lemocla@icloud.com>
2021-04-10 22:18:11 +0100

    Update css bookmark & add js for increment total & toggle bookmark

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,39 @@
-/*JS FILE NOW OPENED*/
-
 /*update total money raised*/
 
+document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", incrementTotal);
+
+function incrementTotal() {
+    /* COMMENTS
+        - Pass id of input in function to fetch amount
+        - Update variable to fetch the amount from the right place
+        let amountPledged = parseInt(document.getElementById("backers").innerHTML.replace(",",""))*/
+    let amountPledged = 50;
+    let currentAmount = parseInt(document.getElementById("total-amount").innerHTML.replace(",", ""));
+    let newAmount = currentAmount + amountPledged;
+    document.getElementById("total-amount").innerHTML = newAmount.toLocaleString();
+}
 
 /*incrementation backers by 1*/
 
-let test = document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", incrementBackers);
-
+document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", incrementBackers);
 
 function incrementBackers() {
- 
-    let currentBackers = parseInt(document.getElementById("backers").innerHTML.replace(",",""))
-    console.log(currentBackers);
- let newBackers = ++currentBackers;
- document.getElementById("backers").innerHTML = newBackers.toLocaleString();
+    let currentBackers = parseInt(document.getElementById("backers").innerHTML.replace(",", ""))
+    let newBackers = ++currentBackers;
+    document.getElementById("backers").innerHTML = newBackers.toLocaleString();
 }
 
 /*toggle whether product is bookmarked*/
 
+document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", function () {
+    /*https://redstapler.co/toggle-active-button-state-javascript/*/
+    (this.classList.contains("active")) ? this.classList.remove("active"): this.classList.add("active");
+});
 
 /*progress bar*/
+
+/* modals */
+
 const modalBtn = document.getElementById("modal-btn");
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
