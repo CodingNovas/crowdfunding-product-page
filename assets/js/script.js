6953b5723101fadc6c9413a97cd80cfa21b5d083
CarlosSiendones <carlosbarrerosiendones@gmail.com>
2021-04-11 19:49:42 +0200

    html , css and javascript for progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+
+/*on load*/
+let root = document.documentElement;
+let currentAmount0 = parseInt(document.getElementById("total-amount").innerHTML.replace(",", ""));
+let widthCalc = (currentAmount0 / 100000)*100
+root.style.setProperty('--Width', widthCalc + "%");
+
+
 /*update total money raised*/
 
 document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", incrementTotal);
@@ -7,10 +15,16 @@ function incrementTotal() {
         - Pass id of input in function to fetch amount
         - Update variable to fetch the amount from the right place
         let amountPledged = parseInt(document.getElementById("backers").innerHTML.replace(",",""))*/
+    let target = 100000;
     let amountPledged = 50;
     let currentAmount = parseInt(document.getElementById("total-amount").innerHTML.replace(",", ""));
     let newAmount = currentAmount + amountPledged;
     document.getElementById("total-amount").innerHTML = newAmount.toLocaleString();
+    let newWidth  = (newAmount/target)*100;
+    
+
+    root.style.setProperty('--Width', newWidth + "%");
+
 }
 
 /*incrementation backers by 1*/
@@ -31,6 +45,7 @@ document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", fun
 });
 
 /*progress bar*/
+
 
 /* modals */
 
