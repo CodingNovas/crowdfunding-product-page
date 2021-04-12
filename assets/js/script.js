c3b405454ccfdd80cc08625a1c310d90a60c99be
Claire Lemonnier <lemocla@icloud.com>
2021-04-12 22:42:21 +0100

    Move increment total & backers on total,  plus file cleaning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,65 @@
 //localStorage.removeItem("totalAmount");
 //localStorage.removeItem("backers");
-// Check browser support
 
-if (typeof(Storage) !== "undefined") {
+/* ----------------------- [on load] ------------------------------*/
+
+//Retrieve info from local storage 
+if (typeof (Storage) !== "undefined") {
     // Retrieve total amount
     if (localStorage.totalAmount) {
         document.getElementById("total-amount").innerHTML = localStorage.getItem("totalAmount");
         console.log("logging localStorage total amount = " + localStorage.totalAmount)
-      } else {
+    } else {
         document.getElementById("total-amount").innerHTML = 10;
-       
-      }
+
+    }
     // Retrieve backers
     if (localStorage.backers) {
         document.getElementById("backers").innerHTML = localStorage.getItem("backers");
         console.log("logging localStorage backers = " + localStorage.backers)
-      } else {
+    } else {
         document.getElementById("backers").innerHTML = 5;
-      }
+    }
 
-  } else {
+} else {
     document.getElementById("total-amount").innerHTML = 0;
     document.getElementById("backers").innerHTML = 0;
-  }
+}
 
-
-/*on load*/
+// Progress bar style on load
 let root = document.documentElement;
 let currentAmount0 = parseInt(document.getElementById("total-amount").innerHTML.replace(",", ""));
-let widthCalc = (currentAmount0 / 100000)*100
+let widthCalc = (currentAmount0 / 100000) * 100
 root.style.setProperty('--Width', widthCalc + "%");
 
+/*--------------- [increment total amounts & backer] -------------- */
 
-/*update total money raised*/
+let pledge = document.getElementsByClassName("continue");
+for (let i = 0; i < pledge.length; i++) {
+    document.getElementsByClassName("continue")[i].addEventListener("click", function () {
+        //Fetch amount from imput 
+        let amount = parseInt(document.getElementsByClassName("continue")[i].previousElementSibling.value);
+        incrementTotal(amount);
+        incrementBackers();
+    })
+}
 
-document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", incrementTotal);
+/*----------------- [update total money raised] ------------------*/
 
-function incrementTotal() {
-    /* COMMENTS
-        - Pass id of input in function to fetch amount
-        - Update variable to fetch the amount from the right place
-        let amountPledged = parseInt(document.getElementById("backers").innerHTML.replace(",",""))*/
+function incrementTotal(amount) {
     let target = 100000;
-    let amountPledged = 50;
+    let amountPledged = parseInt(amount);
     let currentAmount = parseInt(document.getElementById("total-amount").innerHTML.replace(",", ""));
     let newAmount = currentAmount + amountPledged;
     document.getElementById("total-amount").innerHTML = newAmount.toLocaleString();
     /*progress bar*/
-    let newWidth  = (newAmount/target)*100;
+    let newWidth = (newAmount / target) * 100;
     root.style.setProperty('--Width', newWidth + "%");
     // Update local storage
     localStorage.setItem("totalAmount", newAmount);
 }
 
-/*incrementation backers by 1*/
-
-document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", incrementBackers);
+/*------------------ [incrementation backers by 1] -----------------*/
 
 function incrementBackers() {
     let currentBackers = parseInt(document.getElementById("backers").innerHTML.replace(",", ""))
@@ -65,16 +69,17 @@ function incrementBackers() {
     localStorage.setItem("backers", newBackers);
 }
 
-/*toggle whether product is bookmarked*/
+/*------------------- [toggle whether product is bookmarked] ------------------*/
 
 document.getElementsByClassName("btn-bookmark")[0].addEventListener("click", function () {
     /*https://redstapler.co/toggle-active-button-state-javascript/*/
     (this.classList.contains("active-bookmark")) ? this.classList.remove("active-bookmark"): this.classList.add("active-bookmark");
 });
 
-/*countdown*/
-let countDownDate = new Date ("Aug 11, 2021 00:00:00").getTime();
-var x = setInterval(function() {
+/*------------------------- [countdown] -----------------------------*/
+
+let countDownDate = new Date("Aug 11, 2021 00:00:00").getTime();
+var x = setInterval(function () {
     let currentDate = new Date().getTime();
     let distance = countDownDate - currentDate;
 
@@ -83,19 +88,19 @@ var x = setInterval(function() {
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
     console.log();
 
-        if (distance < 0) {
+    if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "THANK YOU FOR YOUR SUPPORT!!!";
-      }
-    }, 1000);
+    }
+}, 1000);
 
 
 
-/* modals */
+/*----------------------- [ modals ] ---------------------------*/
 
 const modalBtn = document.getElementById("modal-btn");
 const modal = document.getElementById("modal");
@@ -128,14 +133,13 @@ function closeModal() {
 }
 
 let pledgeCardsInfo = pledgeCardsData();
-for (let i=0; i < pledgeCardsInfo.length; i++) {
-    for (let j=0; j < pledgeCardsInfo[i].element.children.length - 1; j++) {
-        pledgeCardsInfo[i].element.children[j].addEventListener("click", function() {
+for (let i = 0; i < pledgeCardsInfo.length; i++) {
+    for (let j = 0; j < pledgeCardsInfo[i].element.children.length - 1; j++) {
+        pledgeCardsInfo[i].element.children[j].addEventListener("click", function () {
             if (!pledgeCardsInfo[i].isOpen) {
                 pledgeCardsInfo[i].element.classList.add("open");
                 pledgeCardsInfo[i].isOpen = true;
-            }
-            else if (pledgeCardsInfo[i].isOpen) {
+            } else if (pledgeCardsInfo[i].isOpen) {
                 pledgeCardsInfo[i].element.classList.remove("open");
                 pledgeCardsInfo[i].isOpen = false;
             }
