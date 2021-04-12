6480aaf861dcc7a4a15501a2ef71e4ebc7ce2ee8
Claire Lemonnier <lemocla@icloud.com>
2021-04-12 18:01:44 +0100

    Web API for total amount & backers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+//localStorage.removeItem("totalAmount");
+//localStorage.removeItem("backers");
+// Check browser support
+
+if (typeof(Storage) !== "undefined") {
+    // Retrieve total amount
+    if (localStorage.totalAmount) {
+        document.getElementById("total-amount").innerHTML = localStorage.getItem("totalAmount");
+        console.log("logging localStorage total amount = " + localStorage.totalAmount)
+      } else {
+        document.getElementById("total-amount").innerHTML = 10;
+       
+      }
+    // Retrieve backers
+    if (localStorage.backers) {
+        document.getElementById("backers").innerHTML = localStorage.getItem("backers");
+        console.log("logging localStorage backers = " + localStorage.backers)
+      } else {
+        document.getElementById("backers").innerHTML = 5;
+      }
+
+  } else {
+    document.getElementById("total-amount").innerHTML = 0;
+    document.getElementById("backers").innerHTML = 0;
+  }
+
 
 /*on load*/
 let root = document.documentElement;
@@ -20,11 +46,11 @@ function incrementTotal() {
     let currentAmount = parseInt(document.getElementById("total-amount").innerHTML.replace(",", ""));
     let newAmount = currentAmount + amountPledged;
     document.getElementById("total-amount").innerHTML = newAmount.toLocaleString();
+    /*progress bar*/
     let newWidth  = (newAmount/target)*100;
-    
-
     root.style.setProperty('--Width', newWidth + "%");
-
+    // Update local storage
+    localStorage.setItem("totalAmount", newAmount);
 }
 
 /*incrementation backers by 1*/
@@ -35,6 +61,8 @@ function incrementBackers() {
     let currentBackers = parseInt(document.getElementById("backers").innerHTML.replace(",", ""))
     let newBackers = ++currentBackers;
     document.getElementById("backers").innerHTML = newBackers.toLocaleString();
+    //Update local storage
+    localStorage.setItem("backers", newBackers);
 }
 
 /*toggle whether product is bookmarked*/
